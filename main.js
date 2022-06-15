d10d98f57840d09a4f619cbff583c52b271294de
Jeaneth Tafur <jtafurh1204@gmail.com>
2022-06-14 22:35:30 -0500

    Clase: Mostrando gatitos aleatoreamente

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ console.log('Hola mundo');
 
 const URL_API = 'https://api.thecatapi.com/v1/images/search';
 
-
-const recargar2 = async()=>{
+async function recargar2 (){
     try {
         const response = await fetch(URL_API);
         const data = await response.json()
@@ -14,14 +13,14 @@ const recargar2 = async()=>{
     }
 }
 recargar2()
+/* Con Promesas */
+// function recargar () {
+//     fetch(URL_API)
+//         .then(response => response.json())
+//         .then(data => {
+//             const img = document.getElementById('img-cat');
+//             img.src = data[0].url;
+//         })
+// }
 
-function recargar () {
-    fetch(URL_API)
-        .then(response => response.json())
-        .then(data => {
-            const img = document.getElementById('img-cat');
-            img.src = data[0].url;
-        })
-}
-
-//recargar()
+// recargar()
